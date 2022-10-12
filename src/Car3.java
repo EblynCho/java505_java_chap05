c29cd2b862717e39d111d9d980a1b9c5d9d01c07
eblyncho <eblyn52@naver.com>
2022-10-12 16:18:20 +0900

    다형성이란?

--- src/Car3.java
+++ src/Car3.java
@@ -1,6 +1,6 @@
 public class Car3 {
     Tire[] tires = {
-            new Tire("앞왼쪽", 6),
+            new Tire("앞왼쪽", 6),  // 빈 공간 null 에 데이터 넣으려면 new 키워드로 생성
             new Tire("앞오른쪽", 2),
             new Tire("뒤왼쪽", 3),
             new Tire("뒤오른쪽", 4)
